c05147acf6dc7ad5d6d0250bb2093967738c101e
Park Ju Hyung <qkrwngud825@gmail.com>
2015-09-05 22:18:53 +0900

    vidc: add lazyplug hooks
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -24,6 +24,8 @@
 
 #define MAX_EVENTS 30
 
+extern void lazyplug_enter_lazy(bool enter);
+
 static int get_poll_flags(void *instance)
 {
 	struct msm_vidc_inst *inst = instance;
@@ -1341,6 +1343,8 @@ void *msm_vidc_open(int core_id, int session_type)
 
 	setup_event_queue(inst, &core->vdev[session_type].vdev);
 
+	lazyplug_enter_lazy(true);
+
 	return inst;
 fail_init:
 	vb2_queue_release(&inst->bufq[OUTPUT_PORT].vb2_bufq);
@@ -1466,6 +1470,8 @@ int msm_vidc_close(void *instance)
 	pr_info(VIDC_DBG_TAG "Closed video instance: %p\n",
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
 	kfree(inst);
+
+	lazyplug_enter_lazy(false);
 
 	return 0;
 }
